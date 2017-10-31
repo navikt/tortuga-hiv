8ce104639e81b28d96bdcc554144266a0e9086b5
David Steinsland <github@davidsteinsland.net>
2017-10-31 08:37:34 +0100

    Made HendelserApi more general, not tied to PGI-hendelser

--- hiv/src/main/java/no/nav/opptjening/hiv/hendelser/SkattConfiguration.java
+++ hiv/src/main/java/no/nav/opptjening/hiv/hendelser/SkattConfiguration.java
@@ -1,5 +1,6 @@
 package no.nav.opptjening.hiv.hendelser;
 
+import no.nav.opptjening.skatt.api.SkatteetatenClient;
 import no.nav.opptjening.skatt.api.hendelser.Hendelser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +10,8 @@ import org.springframework.context.annotation.Configuration;
 public class SkattConfiguration {
 
     @Bean
-    public Hendelser hendelser(@Value("${skatt.api.url}") String baseurl, @Value("${skatt.api.hendelser-path}") String endpoint) {
-        return new Hendelser(baseurl + endpoint);
+    public Hendelser hendelser(@Value("${skatt.api.url}") String baseurl) {
+        SkatteetatenClient skatteetatenClient = new SkatteetatenClient(baseurl);
+        return skatteetatenClient.getInntektHendelser();
     }
 }
