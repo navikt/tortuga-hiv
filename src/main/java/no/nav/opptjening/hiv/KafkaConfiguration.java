b769ea2d1a21ce798c404e2b0f4ddd1e8e4038a4
Marius Eriksen <Marius.Eriksen@nav.no>
2018-08-31 10:56:19 +0200

    Changed config to preserve ordering of messages

--- src/main/java/no/nav/opptjening/hiv/KafkaConfiguration.java
+++ src/main/java/no/nav/opptjening/hiv/KafkaConfiguration.java
@@ -102,7 +102,7 @@ public class KafkaConfiguration {
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
 
-        configs.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
+        configs.put(ProducerConfig.RETRIES_CONFIG, 0);
         configs.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1);
 
         return new KafkaProducer<>(configs);
