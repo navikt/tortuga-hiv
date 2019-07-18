74215577f072c1d087e2ab064ac920b5ba25b69f
Marius Eriksen <marius.eriksen@nav.no>
2019-07-18 09:31:55 +0200

    Correct indentation

--- src/main/java/no/nav/opptjening/hiv/SaslSecurityConfig.java
+++ src/main/java/no/nav/opptjening/hiv/SaslSecurityConfig.java
@@ -8,26 +8,27 @@ import java.util.Map;
 
 public class SaslSecurityConfig implements KafkaConfiguration.KafkaSecurtyConfig {
 
-        class Properties {
-            static final String USERNAME = "KAFKA_USERNAME";
-            static final String PASSWORD = "KAFKA_PASSWORD";
-        }
-        private final String saslJaasConfig;
-
-        SaslSecurityConfig(Map<String, String> env) {
-            this.saslJaasConfig = createPlainLoginModule(env.get(Properties.USERNAME), env.get(Properties.PASSWORD));
-        }
-        
-        private String createPlainLoginModule(String username, String password) {
-            return String.format("org.apache.kafka.common.security.plain.PlainLoginModule required username=\"%s\" password=\"%s\";", username, password);
-        }
-
-        @Override
-        public Map<String, Object> getSecurityConfig() {
-            Map<String, Object> configs = new HashMap<>();
-            configs.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
-            configs.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
-            configs.put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
-            return configs;
-        }
+    class Properties {
+        static final String USERNAME = "KAFKA_USERNAME";
+        static final String PASSWORD = "KAFKA_PASSWORD";
     }
+
+    private final String saslJaasConfig;
+
+    SaslSecurityConfig(Map<String, String> env) {
+        this.saslJaasConfig = createPlainLoginModule(env.get(Properties.USERNAME), env.get(Properties.PASSWORD));
+    }
+
+    private String createPlainLoginModule(String username, String password) {
+        return String.format("org.apache.kafka.common.security.plain.PlainLoginModule required username=\"%s\" password=\"%s\";", username, password);
+    }
+
+    @Override
+    public Map<String, Object> getSecurityConfig() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+        configs.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+        configs.put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
+        return configs;
+    }
+}
